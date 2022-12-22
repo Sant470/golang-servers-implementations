871fd88ee371f7c04f64ec99359ba55d279d4bb8
Sant Purushotam <sant.purushotam@thefastway.in>
2022-12-23 02:27:11 +0530

    feature: use default servemux

---
+++ default-serve-mux/main.go
@@ -0,0 +1,40 @@
+/*
+Using the global instance of ServeMux which is explicitly provided by net http package.
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type courses map[string]int
+
+func (c courses) list(rw http.ResponseWriter, r *http.Request) {
+	for course, price := range c {
+		fmt.Fprintf(rw, "course: %s, price: %d\n", course, price)
+	}
+}
+
+func (c courses) price(rw http.ResponseWriter, r *http.Request) {
+	course := r.URL.Query().Get("course")
+	price, ok := c[course]
+	if !ok {
+		rw.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(rw, "course not available!")
+		return 
+	}
+	fmt.Fprintf(rw, "price: %d", price)
+}
+
+func main() {
+	c := courses{
+		"golang-beginner": 1000,
+		"golang-intermediate": 5000,
+		"golang-advanced": 10000,
+	}
+	http.Handle("/list", http.HandlerFunc(c.list))
+	http.Handle("/price", http.HandlerFunc(c.price))
+	http.ListenAndServe("localhost:8000", nil)
+}
